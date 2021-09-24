4d3c7b889ea57bef2c05d9c5905e534cd0f75769
booiris <954520770@qq.com>
2021-09-24 21:40:16 +0800

    update

---
+++ server.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+
+max_connection = 20
+
+
+############    主线程
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+host_name = socket.gethostname()
+host_name = socket.gethostbyname(host_name)
+address = (host_name, 8888)
+server_socket.bind(address)
+server_socket.settimeout(120)
+server_socket.listen()
+############
+
+class worker(threading.Thread):
+    def __init__(self, request_id):
+        self.start()
+    def work(self, tcp_socket)
+        pass
+
+while True:
+    try: 
+        client,addr = server_socket.accept()
+
+    except socket.timeout:
+        print("main server timeout")
+

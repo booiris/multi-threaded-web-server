8c34742a2f0c0e2ca8d1e1ccb17c96f105609397
booiris <954520770@qq.com>
2021-09-25 09:30:03 +0800

    update server and client

---
+++ client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+port = input()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_name = socket.gethostbyname(host_name)
+address = (host_name, int(port))
+client_socket.bind(address)
+client_socket.settimeout(120)
+client_socket.connect((host_name,8888))
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ max_connection = 20
 
 
 ############    主线程
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name = socket.gethostname()
 host_name = socket.gethostbyname(host_name)
 address = (host_name, 8888)
@@ -18,13 +18,13 @@ server_socket.listen()
 class worker(threading.Thread):
     def __init__(self, request_id):
         self.start()
-    def work(self, tcp_socket)
+    def work(self, tcp_socket):
         pass
 
 while True:
     try: 
-        client,addr = server_socket.accept()
-
+        clie?!?jedi=0, nt,addr = server_socket.accept()?!? (*_**values: object*_*, sep: Optional[str]=..., end: Optional[str]=..., file: Optional[SupportsWrite[str]]=..., flush: bool=...) ?!?jedi?!?
+        print(client.getpeername(),client.gethostname)
     except socket.timeout:
         print("main server timeout")
 
